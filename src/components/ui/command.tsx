8619c335124d88fde5a4f0684fd12d4ee46ecb42
tblt-gr <thibault.grosjean1@gmail.com>
2026-05-26 10:45:44 +0200

    fix: adjust padding for command input wrapper component

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -61,7 +61,7 @@ function CommandInput({
   ...props
 }: React.ComponentProps<typeof CommandPrimitive.Input>) {
   return (
-    <div data-slot="command-input-wrapper" className="p-1 pb-0">
+    <div data-slot="command-input-wrapper" className="p-0">
       <InputGroup className="h-8! rounded-lg! border-input/30 bg-input/30 shadow-none! *:data-[slot=input-group-addon]:pl-2!">
         <CommandPrimitive.Input
           data-slot="command-input"
