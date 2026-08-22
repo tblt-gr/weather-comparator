3b443b2633de7d4f256df1d0cbeb721c85f983bd
tblt-gr <62742711+tblt-gr@users.noreply.github.com>
2026-08-22 20:24:19 +0200

    fix: round PWA and browser icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -42,7 +42,7 @@ export default async function manifest(): Promise<MetadataRoute.Manifest> {
         purpose: "any",
       },
       {
-        src: "/icon-512.png",
+        src: "/icon-maskable-512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
